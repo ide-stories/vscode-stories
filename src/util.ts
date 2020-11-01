711f4a1fb3d3f3511d8c7eb8323c92f9e53db6ef
LiangjunSong <liangjun_song@yeah.net>
2020-11-01 04:23:00 +0000

    [Feature]Dislay story created time

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import * as path from "path";
 
+const dateFormatLessThanAYear = { month: 'long', day: 'numeric' };
+const dateFormatDefault = { year: 'numeric', month: 'long', day: 'numeric' }
+
 // https://github.com/arciisine/vscode-chronicler/blob/master/src/util.ts
 export class Util {
   static context: vscode.ExtensionContext;
@@ -14,5 +17,24 @@ export class Util {
     return path
       .resolve(this.context.extensionPath, rel.replace(/\//g, path.sep))
       .replace(/\\/g, "/");
+  }
+
+  static getCreatedTimeString(date: Date): string {
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerYear = msPerDay * 365;
+
+    const elapsed = (new Date()).getTime() - date.getTime();
+
+    if (elapsed < msPerHour) {
+      return Math.floor(elapsed / msPerMinute) + ' minutes ago';
+    } else if (elapsed < msPerDay) {
+      return Math.floor(elapsed / msPerHour) + ' hours ago';
+    } else if (elapsed < msPerYear) {
+      return date.toLocaleDateString(undefined, dateFormatLessThanAYear);
+    } else {
+      return date.toLocaleDateString(undefined, dateFormatDefault);
+    }
   }
 }
