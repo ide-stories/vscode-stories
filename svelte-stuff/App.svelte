faaab27d0a5c60d1ffc2f08b4e09d3c138242ed7
Ben Awad <benawad97@gmail.com>
2020-11-04 12:32:54 -0600

    svelte conversion done

--- svelte-stuff/App.svelte
+++ svelte-stuff/App.svelte
@@ -1,36 +1,104 @@
 <script lang="ts">
-  import StoryBubble from "./StoryBubble.svelte";
-  import type { TextStoryListResponse } from "./types";
+  import { onMount } from "svelte";
 
-  const fetchStories: Promise<TextStoryListResponse> = (async () => {
-    const response = await fetch(`${apiBaseUrl}/text-stories/hot`);
-    return await response.json();
-  })();
+  import StoryBubble from "./StoryBubble.svelte";
+  import type { TextStoryListItem, TextStoryListResponse } from "./types";
+
+  let loadingState: "initial" | "more" | "refetch" | "ready" = "initial";
+
+  let cursor = 0;
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${apiBaseUrl}/text-stories/hot` + (cursor ? `/${cursor}` : "")
+      );
+      const d = await response.json();
+      const ids = new Set();
+      const newStories = [];
+      if (loadingState !== "refetch") {
+        stories.forEach((s) => {
+          newStories.push(s);
+          ids.add(s.id);
+        });
+      }
+      for (const s of d.stories) {
+        if (!ids.has(s.id)) {
+          newStories.push(s);
+          ids.add(s.id);
+        }
+      }
+      stories = newStories;
+      hasMore = d.hasMore;
+      cursor++;
+    } catch (err) {
+      error = err;
+    }
+    loadingState = "ready";
+  };
+
+  let stories: TextStoryListItem[] = [];
+  let hasMore = false;
+  let error = null;
+
+  onMount(async () => {
+    await fetchData();
+  });
+
+  window.addEventListener("message", (event) => {
+    const message = event.data;
+    switch (message.command) {
+      case "new-story":
+        if (stories.every((x) => x.id !== message.story.id)) {
+          stories = [message.story, ...stories];
+        }
+        break;
+      case "refresh":
+        if (loadingState === "ready") {
+          cursor = 0;
+          loadingState = "refetch";
+          fetchData();
+        }
+        break;
+    }
+  });
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  button {
+    margin-bottom: 20px;
   }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .story-grid {
+    display: grid;
+    gap: 10px;
+    justify-content: space-around;
+    grid-template-columns: repeat(auto-fit, 60px);
+    margin-top: 5px;
+    margin-bottom: 20px;
   }
 </style>
 
 <main>
-  {#await fetchStories}
-    <p>loading stories...</p>
-  {:then data}
-    {#each data.stories as story}
-      <StoryBubble name={story.creatorUsername} />
-    {/each}
-  {:catch error}
+  {#if error}
     <p>An error has occured: {error.message}</p>
-  {/await}
+  {:else if loadingState === 'initial' || loadingState === 'refetch'}
+    <p>loading stories...</p>
+  {:else}
+    <div class="story-grid">
+      {#each stories as story}
+        <StoryBubble
+          onClick={() => {
+            tsvscode.postMessage({ type: 'onStoryPress', value: story.id });
+          }}
+          {...story} />
+      {/each}
+    </div>
+    {#if hasMore}
+      <button
+        disabled={loadingState !== 'ready'}
+        on:click={() => {
+          loadingState = 'more';
+          fetchData();
+        }}>load more</button>
+    {/if}
+  {/if}
 </main>
--- svelte-stuff/globals.d.ts
+++ svelte-stuff/globals.d.ts
@@ -1,7 +1,7 @@
 import * as _vscode from "vscode";
 
 declare global {
-  const tsvscode: typeof _vscode;
+  const tsvscode: any;
   const flairMap: Record<string, string>;
   const apiBaseUrl: string;
 }
