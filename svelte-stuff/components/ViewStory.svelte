397808e217d803f91e8802715616175d114dd852
Fernando B <fernandobe+git@protonmail.com>
2020-12-13 14:10:39 -0600

    hide-friendButton-forCurrentUser

--- svelte-stuff/components/ViewStory.svelte
+++ svelte-stuff/components/ViewStory.svelte
@@ -134,7 +134,7 @@
         <line x1="10" y1="11" x2="10" y2="17" />
         <line x1="14" y1="11" x2="14" y2="17" /></svg>
     {/if}
-    {#if !isFriend}
+    {#if !isFriend && currentUserId !== textStory.creatorId}
       <svg
         on:click={async () => {
           try {
@@ -151,7 +151,7 @@
             fill="white"
             d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm0 8c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4zm-6 4c.22-.72 3.31-2 6-2 2.7 0 5.8 1.29 6 2H9zm-3-3v-3h3v-2H6V7H4v3H1v2h3v3z" />
         </g></svg>
-    {:else}
+    {:else if currentUserId !== textStory.creatorId}
       <svg
         on:click={async () => {
           await mutation(`/remove-friend/${textStory.creatorId}`, {});
